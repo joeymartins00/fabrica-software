b8d2c0526574255e495a7978509fb1edf82cc6cb
Joceyr Gomes Martins <119597501+joeymartins00@users.noreply.github.com>
2023-02-01 16:07:03 -0400

    Update Exercício 83 - Escreva um algoritmo que solicite ao usuário a entrada de 5 números.py

---
+++ Exercício 83 - Escreva um algoritmo que solicite ao usuário a entrada de 5 números.py
@@ -0,0 +1,1 @@
+Escreva um algoritmo que solicite ao usuário a entrada de 5 números, e que exiba o somatório desses números na tela. Após exibir a soma, o programa deve mostrar também os números que o usuário digitou, um por linha.​
