700f45f8d2ca15294d0bb8460ca2627c350ba53e
Joceyr Gomes Martins <119597501+joeymartins00@users.noreply.github.com>
2023-02-01 16:06:04 -0400

    Update Exercício 81 - Considere os cenários de problemas a serem resolvidos.py

---
+++ Exercício 81 - Considere os cenários de problemas a serem resolvidos.py
@@ -0,0 +1,19 @@
+Considere os cenários de problemas a serem resolvidos:​
+
+1- Um berçário deseja informatizar suas operações.​
+
+Quando um bebê nasce, algumas informações são armazenadas sobre ele, tais como: nome, data do nascimento, peso do nascimento, altura, a mãe deste bebê e o médico que fez seu parto. Para as mães, o berçário também deseja manter um controle, guardando informações como: nome, endereço, telefone e data de nascimento. Para os médicos, é importante saber: CRM, nome, telefone celular e especialidade.​
+
+Para resolver o cenário 1 faça:​
+
+A) Levantamento de requisitos (funcionais e não funcionais);​
+
+B) Diagramas UML (Caso de Uso);​
+
+C) Documento com levantamento de requisitos e diagramas UML;​
+
+D) Algoritmo em python.​
+
+E) Apresentação da atividade (Em slides)​
+
+F) Ao finalizar a atividade coloque na pasta compartilhada no Microsoft Teams (crie uma pasta com a descrição “AtivAva1DevAlg” dentro de ambas as pastas dos membros da dupla).
