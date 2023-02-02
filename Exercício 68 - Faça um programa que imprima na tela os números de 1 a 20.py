cc8972ff5a4704770a9f367a69b073e50a42f561
Joceyr Gomes Martins <119597501+joeymartins00@users.noreply.github.com>
2023-02-02 02:39:54 -0300

    Update Exercício 68 - Faça um programa que imprima na tela os números de 1 a 20.py

--- Exercício 68 - Faça um programa que imprima na tela os números de 1 a 20.py
+++ Exercício 68 - Faça um programa que imprima na tela os números de 1 a 20.py
@@ -1,1 +1,5 @@
-Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
+'''Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programa para que ele mostre os números um ao lado do outro.'''
+for i in range (21):
+	print (i)
+print(list(range(1,21)))
