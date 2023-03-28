877be35b34fedc7f72efc5691b680290357faa38
Joceyr <joeymartins00@gmail.com>
2023-03-28 17:01:22 -0400

    atualização do Program do exercício 2

--- package/escola/program.py
+++ package/escola/program.py
@@ -2,5 +2,9 @@ from pessoa import Pessoa
 from pessoa import Aluno
 from pessoa import Professor
 
-a1 = Aluno("Joceyr",32,6071166,10,9.5,10,10)
-a1.calculo_media()
+a1 = Aluno("Joceyr",32,6071166)
+a1.get_media()
+a1.estudar()
+
+p1 = Professor("Thiago Almeida",45, 387556, "Cientista da Computação", "Orientação a Objetos")
+p1.lecionar()
